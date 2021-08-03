09315d31df7e039b24ff7061bbc3c00e33862ca5
Rommel Rios Damian <61754866+Jrios28@users.noreply.github.com>
2021-08-03 01:44:18 -0500

    avance do projeto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
-import React from 'react'
-import { AppContainer } from './components/AppContainer'
+import React, { Component } from 'react'
+import LabeninjaPrincipal from './components/LabeninjaPrincipal/LabeninjaPrincipal'
+import LabeninjaContrato from './components/LabeninjaContratos/LabeninjaContrato'
+import LabeninjaCadastro from './components/LabeninjaCadastro/LabeninjaCadastro'
 
-function App() {
-	return (
-        <AppContainer />
-	)
+
+
+export default class App extends React.Component {
+
+		state = {
+			pagina : "Principal"
+		}
+
+		renderizaPagina = () => {
+			switch(this.state.pagina){
+				case "Cadastro":
+					return <LabeninjaCadastro 
+							irParaPrincipal={this.irParaPrincipal}
+							/>
+				case "Contratos":
+					return <LabeninjaContrato 
+							irParaPrincipal={this.irParaPrincipal}
+							/>
+				case "Principal":
+					return <LabeninjaPrincipal
+							irParaCadastro = {this.irCadastro}
+							irParaContrato = {this.irContratos}
+							/>	
+			}
+		}
+
+		irCadastro = () => {
+			console.log("Estou dentro do metodo Cadastro")
+			this.setState({ pagina : "Cadastro"});
+		}
+		
+		irContratos = () => {
+			console.log("Estou dentro do metodo Contratos")
+			this.setState({ pagina : "Contratos"});
+		}
+		irParaPrincipal = () => {
+			console.log("Estou dentro do metodo principal")
+			this.setState({ pagina : "Principal"})
+		}
+
+	render(){
+		return (		
+			<div>
+				{this.renderizaPagina()}
+			</div>
+
+		)
+
+	}
+
 }
 
-export default App
+
