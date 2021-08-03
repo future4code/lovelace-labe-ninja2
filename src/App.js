e7eb053159a74b879964d8856c31cf1e4009e315
Brenda Simionato <brendaevellyn97@gmail.com>
2021-08-03 11:50:46 -0300

    Adicionado detalhes dos produtos

--- src/App.js
+++ src/App.js
@@ -1,58 +1,100 @@
-import React, { Component } from 'react'
+import React from 'react'
 import LabeninjaPrincipal from './components/LabeninjaPrincipal/LabeninjaPrincipal'
 import LabeninjaContrato from './components/LabeninjaContratos/LabeninjaContrato'
 import LabeninjaCadastro from './components/LabeninjaCadastro/LabeninjaCadastro'
+import axios from 'axios'
+import styled from 'styled-components'
+import Detalhes from './components/Detalhes'
+
+const MainContainer = styled.div`
+
+`
 
 
 
 export default class App extends React.Component {
 
-		state = {
-			pagina : "Principal"
-		}
+	state = {
 
-		renderizaPagina = () => {
-			switch(this.state.pagina){
-				case "Cadastro":
-					return <LabeninjaCadastro 
-							irParaPrincipal={this.irParaPrincipal}
-							/>
-				case "Contratos":
-					return <LabeninjaContrato 
-							irParaPrincipal={this.irParaPrincipal}
-							/>
-				case "Principal":
-					return <LabeninjaPrincipal
-							irParaCadastro = {this.irCadastro}
-							irParaContrato = {this.irContratos}
-							/>	
-			}
-		}
-
-		irCadastro = () => {
-			console.log("Estou dentro do metodo Cadastro")
-			this.setState({ pagina : "Cadastro"});
-		}
+		pagina: "Principal",
 		
-		irContratos = () => {
-			console.log("Estou dentro do metodo Contratos")
-			this.setState({ pagina : "Contratos"});
-		}
-		irParaPrincipal = () => {
-			console.log("Estou dentro do metodo principal")
-			this.setState({ pagina : "Principal"})
-		}
+		produto: {}
 
-	render(){
-		return (		
-			<div>
+	}
+
+
+
+	detalhesProduto = (id) => {
+
+		const url = "https://labeninjas.herokuapp.com/jobs/" + id
+		const authorization = "089c363c-2449-490e-9e13-234a13327ac2"
+
+		axios.get(url, {
+			headers: {
+				authorization
+			}
+		}).then((resp) => {
+			this.setState({ produto: resp.data })
+
+		}).catch((error) => {
+			alert("Erro ao exibir detalhes!")
+
+		})
+
+	}
+
+
+
+	renderizaPagina = () => {
+		switch (this.state.pagina) {
+			case "Cadastro":
+				return <LabeninjaCadastro
+					irParaPrincipal={this.irParaPrincipal}
+				/>
+			case "Contratos":
+				return <LabeninjaContrato
+					irParaPrincipal={this.irParaPrincipal}
+				/>
+			case "Principal":
+				return <LabeninjaPrincipal
+					irParaCadastro={this.irCadastro}
+					irParaContrato={this.irContratos}
+				/>
+		}
+	}
+
+
+
+	irCadastro = () => {
+		console.log("Estou dentro do metodo Cadastro")
+		this.setState({ pagina: "Cadastro" });
+	}
+
+	irContratos = () => {
+		console.log("Estou dentro do metodo Contratos")
+		this.setState({ pagina: "Contratos" });
+	}
+	irParaPrincipal = () => {
+		console.log("Estou dentro do metodo principal")
+		this.setState({ pagina: "Principal" })
+	}
+
+
+	
+
+	render() {
+		return (
+			<MainContainer>
+
 				{this.renderizaPagina()}
-			</div>
+
+				{/* <button type="text" onClick={() => this.detalhesProduto("175cb9cb-e680-4f9b-8fa8-a0addf65523f")}>Exibir detalhes</button>
+				<Detalhes produto={this.state.produto}></Detalhes> */}
+
+			</MainContainer>
 
 		)
 
 	}
 
 }
-
-
---
+++ src/components/Detalhes.js
@@ -0,0 +1,39 @@
+import React from "react"
+import styled from "styled-components"
+
+const Container = styled.div`
+display: inline;
+`
+
+
+export default class Detalhes extends React.Component {
+
+
+
+    render() {
+        return (
+            <Container>
+                <div>
+                    <b>Título:</b> {this.props.produto.title}
+                    <p></p>
+                </div>
+                <div>
+                    <b>Descrição:</b> {this.props.produto.description}
+                    <p></p>
+                </div>
+                <div>
+                    <b>Preço:</b> {this.props.produto.price}
+                    <p></p>
+                </div>
+                <div>
+                    <b>Formas de Pagamento:</b>{this.props.produto.paymentMethods}
+                    <p></p>
+                </div>
+
+                <button type="text">Adicionar ao Carrinho</button>
+                <button type="text">Voltar para Lista</button>
+            </Container>
+        )
+    }
+
+}
